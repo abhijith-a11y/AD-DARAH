c4091f3be4d9c680671d9194b7a02698b1649c1c
abhijith-a11y <abhijith@lement8.ae>
2026-01-17 17:11:37 +0530

    news slider completed

--- template-parts/NewsArticles.php
+++ template-parts/NewsArticles.php
@@ -12,55 +12,70 @@
 
 <section class="news-articles-container">
 	<div class="container text-center">
-		 <div class="title-block ">
-			<h5 >News & Articles</h5>
-            <h2  >What’s New Happening</h2>
+		<div class="title-block ">
+			<h5>News & Articles</h5>
+			<h2>What’s New Happening</h2>
 			<a href="#" class="buttion primary-button">
-         Explore More
-           <span class="su_button_circle desplode-circle" style="left: 110px; top: 292.438px;"></span></a>
-		 </div>
+				Explore More
+				<span class="su_button_circle desplode-circle" style="left: 110px; top: 292.438px;"></span></a>
+		</div>
 	</div>
 
 
 
 	<div class="news-articles-slider-wrapper">
-			<div class="swiper news-articles-swiper" data-news-articles-swiper>
-				<div class="swiper-wrapper">
-					<!-- Swiper Slides - Static Data -->
-					<?php
-					$news_images = array(
-						get_template_directory_uri() . '/assets/images/news_01.png',
-						get_template_directory_uri() . '/assets/images/news_02.png',
-						get_template_directory_uri() . '/assets/images/news_03.png',
-					);
-					
-					$news_data = array(
-						array('date' => '05 August 2022', 'heading' => 'Inside ADDarah\'s vision for transformative hospitality'),
-						array('date' => '05 August 2022', 'heading' => 'ADDarah and the future of Saudi hospitality'),
-						array('date' => 'August 2022', 'heading' => 'Zouq Al-Harbi: Delivering hospitality projects in line with Vision 2030'),
-					);
-					
-					// Repeat images for multiple slides
-					for ($i = 0; $i < 12; $i++):
-						$image_index = $i % 3;
-						$data_index = $i % 3;
+		<div class="swiper news-articles-swiper" data-news-articles-swiper>
+			<div class="swiper-wrapper">
+				<!-- Swiper Slides - Static Data -->
+				<?php
+				$news_images = array(
+					get_template_directory_uri() . '/assets/images/news_01.png',
+					get_template_directory_uri() . '/assets/images/news_02.png',
+					get_template_directory_uri() . '/assets/images/news_03.png',
+				);
+
+				$news_data = array(
+					array('date' => '05 August 2022', 'heading' => 'Inside ADDarah\'s vision for transformative hospitality'),
+					array('date' => '05 August 2022', 'heading' => 'ADDarah and the future of Saudi hospitality'),
+					array('date' => 'August 2022', 'heading' => 'Zouq Al-Harbi: Delivering hospitality projects in line with Vision 2030'),
+				);
+
+				// Repeat images for multiple slides
+				for ($i = 0; $i < 12; $i++):
+					$image_index = $i % 3;
+					$data_index = $i % 3;
 					?>
 					<div class="swiper-slide">
 						<div class="news-article-card">
-							<div class="news-article-image" style="background-image: url('<?php echo esc_url($news_images[$image_index]); ?>');">
+							<div class="news-article-image"
+								style="background-image: url('<?php echo esc_url($news_images[$image_index]); ?>');">
 								<div class="news-article-overlay"></div>
 								<div class="news-article-content">
 									<div class="news-article-left">
-										<div class="news-article-date"><?php echo esc_html($news_data[$data_index]['date']); ?></div>
-										<h3 class="news-article-heading"><?php echo esc_html($news_data[$data_index]['heading']); ?></h3>
+										<div class="news-article-date">
+											<?php echo esc_html($news_data[$data_index]['date']); ?></div>
+										<h3 class="news-article-heading">
+											<?php echo esc_html($news_data[$data_index]['heading']); ?></h3>
 									</div>
 									<div class="news-article-right">
 										<a href="#" class="news-article-learn-more">
 											<span class="news-article-learn-text">Learn More</span>
 											<span class="news-article-learn-icon">
-												<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-													<path d="M6 12L10 8L6 4" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+												<svg width="34" height="34" viewBox="0 0 34 34" fill="none"
+													xmlns="http://www.w3.org/2000/svg">
+													<rect x="0.617143" y="32.7086" width="32.0914" height="32.0914"
+														rx="16.0457" transform="rotate(-90 0.617143 32.7086)" stroke="currentColor"
+														stroke-width="1.23429" />
+													<path d="M10.0361 16.6636H23.2906" stroke="currentColor" stroke-width="1.23429"
+														stroke-linecap="round" stroke-linejoin="round" />
+													<path d="M19.5035 12.8766L23.2906 16.6636" stroke="currentColor"
+														stroke-width="1.23429" stroke-linecap="round"
+														stroke-linejoin="round" />
+													<path d="M19.5029 20.4507L23.2899 16.6637" stroke="currentColor"
+														stroke-width="1.23429" stroke-linecap="round"
+														stroke-linejoin="round" />
 												</svg>
+
 											</span>
 										</a>
 									</div>
@@ -68,11 +83,11 @@
 							</div>
 						</div>
 					</div>
-					<?php endfor; ?>
-				</div>
-				
-				<!-- Navigation Buttons -->
-				<!-- <div class="news-articles-navigation">
+				<?php endfor; ?>
+			</div>
+
+			<!-- Navigation Buttons -->
+			<!-- <div class="news-articles-navigation">
 					<button class="news-articles-prev-btn" aria-label="Previous slide">
 						<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 							<path d="M15 18L9 12L15 6" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
@@ -84,8 +99,8 @@
 						</svg>
 					</button>
 				</div> -->
-			</div>
 		</div>
+	</div>
 
 
 
@@ -93,4 +108,3 @@
 
 
 </section>
-
